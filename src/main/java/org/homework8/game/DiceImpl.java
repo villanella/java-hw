84552af8580de7dd07781bcabd10a6d3feea5190
Nadezhda Krupenkina <n.krupenkina@arenadata.io>
2023-05-25 22:09:58 +0300

    Initial commit

---
+++ src/main/java/org/homework8/game/DiceImpl.java
@@ -0,0 +1,11 @@
+package org.homework8.game;
+
+import java.util.Random;
+
+public class DiceImpl implements Dice {
+    @Override
+    public int roll() {
+        // https://docs.oracle.com/javase/8/docs/api/java/util/Random.html
+        return new Random().nextInt();
+    }
+}
---
+++ src/main/java/org/homework8/game/GameWinnerPrinter.java
@@ -0,0 +1,5 @@
+package org.homework8.game;
+
+public interface GameWinnerPrinter {
+    void printWinner(Player winner);
+}
