4c04baa15c7b775ae061cbdc38732f4a2f8a178d
Nadezhda Krupenkina <n.krupenkina@arenadata.io>
2023-05-25 22:16:11 +0300

    Initial commit

--- src/main/java/org/homework8/Demo.java
+++ src/main/java/org/homework8/Demo.java
@@ -1,4 +1,13 @@
 package org.homework8;
 
+import org.homework8.game.*;
+
 public class Demo {
+
+    public static void main(String[] args) {
+        Dice dice = new DiceImpl();
+        GameWinnerPrinter winnerPrinter = new GameWinnerConsolePrinter();
+        Game game = new Game(dice, winnerPrinter);
+        game.playGame(new Player("Вася"), new Player("Игорь"));
+    }
 }
